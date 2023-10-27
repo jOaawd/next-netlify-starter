a3aae93cc686eadf98f7cfd7f466aa6d418b901b
jOaawd <70965524+jOaawd@users.noreply.github.com>
2023-10-27 01:29:05 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Google Classroom</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This is Fmodes Netlify App Lancher!</code>
         </p>
       </main>
 
